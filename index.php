7215bb8a55682a1314819554e21460ca8314daa0
Samuel Sandelin <jrsandelin@gmail.com>
2022-12-24 03:22:22 -0800

    minor changes before switching code structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Welcome to the Matrix Portal upload page!</h1>
+<p>Below will be the current display queue as well as settings to change the queue.</p>
+</body>
+</html>
+
+<?php
+	echo "Testing php!";
+	echo $_GET["request_name"];
+?>
+
+<div hidden>
+	test data
+</div>
